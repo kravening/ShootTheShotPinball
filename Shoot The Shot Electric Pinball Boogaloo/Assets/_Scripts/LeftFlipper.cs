279e6f33e118d340a4c74e153815b705710f712b
xraymonx <raymon_jesse2@hotmail.com>
2015-10-05 11:16:59 +0200

    Mijn scene + Left flipper stuff.

---
+++ Shoot The Shot Electric Pinball Boogaloo/Assets/_Scripts/LeftFlipper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftFlipper : MonoBehaviour {
+
+    private int rotationSpeed = 60;
+
+   
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TurningJoint();
+	}
+
+    void TurningJoint()
+    {
+        Debug.Log("hey");
+        if (this.transform.rotation.y < 350)
+        {
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+
+    }
+}
