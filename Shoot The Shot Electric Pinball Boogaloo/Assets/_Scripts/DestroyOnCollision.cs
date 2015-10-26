7e8135d5d51dbbec4672854ecb0a78519a9787d4
18048 <18048@ma-web.nl>
2015-10-26 13:55:32 +0100

    Gates disappear, particle system on collision, replace ball, sounds on collision not functional.

--- Shoot The Shot Electric Pinball Boogaloo/Assets/_Scripts/DestroyOnCollision.cs
+++ Shoot The Shot Electric Pinball Boogaloo/Assets/_Scripts/DestroyOnCollision.cs
@@ -1,22 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class DestroyOnCollision : MonoBehaviour {
 
+	public Transform GateParticle;
+	public AudioClip hit;
+	public AudioSource audio;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		audio = GetComponent<AudioSource>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-
+	
 	void OnCollisionEnter (Collision col)
 	{
 		if(col.gameObject.tag == "ball")
 		{
+			audio.PlayOneShot(hit, 1f);
+			//instantiate particle system op de plaats van de collision
+			Instantiate(GateParticle, col.transform.position, Quaternion.identity);
+			col.transform.position = new Vector3(-26, 0, -15); 
+			//instantiate particle system op de plaats van de nieuwe spawn
+			Instantiate(GateParticle, col.transform.position, Quaternion.identity);
 			Destroy(this.gameObject);
 		}
 	}
