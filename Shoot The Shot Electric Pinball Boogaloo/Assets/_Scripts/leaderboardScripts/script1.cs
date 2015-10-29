3fb09daece8c0f8f99bb22e19f63e2d42852ec2c
xraymonx <raymon_jesse2@hotmail.com>
2015-10-29 20:16:04 +0100

    Leaderboard. Username + score to a textfield. and unity reads it out in a leaderboard scene

---
+++ Shoot The Shot Electric Pinball Boogaloo/Assets/_Scripts/leaderboardScripts/script1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script1 : MonoBehaviour {
+
+
+	public int scoreOfzo = 1;
+	public string name; 
+
+	// Use this for initialization
+	void Start () {
+	
+		string url = "http://17828.hosts.ma-cloud.nl/unityPingPong/unity.php";
+
+		WWWForm form = new WWWForm ();
+
+		form.AddField("Points", scoreOfzo);
+
+		form.AddField("Name", name);
+
+		WWW www = new WWW (url, form);
+
+		StartCoroutine (WaitForRequest (www));
+	}
+
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+
+		if (www.error == null) {
+		
+			Debug.Log ("www ok!: " + www.text);
+		} else {
+		
+			Debug.Log("www error: " + www.error);
+		}
+
+	}
+
+}
---
+++ Shoot The Shot Electric Pinball Boogaloo/Assets/_Scripts/leaderboardScripts/script2.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class script2 : MonoBehaviour {
+	
+	
+	public int scoreOfzo = 1;
+
+    public Text[] fileTexts;
+    
+    // Use this for initialization
+    void Start () {
+		
+		string url = "http://17828.hosts.ma-cloud.nl/unityPingPong/unity2.php";
+		
+		WWW www = new WWW (url);
+		
+		StartCoroutine (WaitForRequest (www));
+
+
+		}
+
+	
+	IEnumerator WaitForRequest(WWW www)
+	{
+		yield return www;
+		
+		if (www.error == null)
+        {
+  
+  
+            ReadIncomingData(www.text);
+
+			Debug.Log ("www ok!: " + www.text);
+
+		} else {
+			
+			Debug.Log("www error: " + www.error);
+            fileTexts[0].text = www.error;
+		}
+		
+	}
+
+
+    public void ReadIncomingData(string incomingString)
+    {
+        string[] splitData = incomingString.Trim().Split('\n');
+
+        if (incomingString == string.Empty)return;
+        
+        int timer = 0;
+        foreach (string entry in splitData)
+        {
+            if (entry.Contains("\t"))
+            {
+                string[] temp = entry.Split('\t');
+
+                string _name = temp[1];
+                string _score = temp[0];
+
+                if (timer < 10)
+                {
+                    fileTexts[0].text += _name + '\n';
+                    fileTexts[1].text += _score + '\n';
+                    timer++;
+                }
+            }
+
+        }
+    }
+}
