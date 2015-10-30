772aa8daf516febca35740c3bb2e001b6cdd65dd
kaj loevesijn <sep_ul_tura@live.nl>
2015-10-30 22:47:57 +0100

    added button functionality.

---
+++ Shoot The Shot Electric Pinball Boogaloo/Assets/_Scripts/TempButtons/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+	[SerializeField]
+	private string level;
+
+	public void ChangeLevel(){
+		Application.LoadLevel(level);
+	}
+}
