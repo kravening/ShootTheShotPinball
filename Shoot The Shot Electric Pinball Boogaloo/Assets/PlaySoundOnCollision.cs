a60c0741cc1d2e83453e6f65f3406266d1391675
kaj loevesijn <sep_ul_tura@live.nl>
2015-10-30 23:41:48 +0100

    really shitty sfx added

---
+++ Shoot The Shot Electric Pinball Boogaloo/Assets/PlaySoundOnCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnCollision : MonoBehaviour {
+	private AudioSource bop;
+
+	void Start(){
+		bop = this.gameObject.GetComponent<AudioSource> ();
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		//Debug.Log("ayy1");
+		if (col.rigidbody != null)
+		{
+			bop.Play();
+		}
+	}
+}
